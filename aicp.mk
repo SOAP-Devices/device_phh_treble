f6a94202d58dcf53874ed1e12d6d5aef6a819c2f
greg kobey <gregkobey@gmail.com>
2018-03-28 15:16:06 -0700

    Update aicp.mk

--- aicp.mk
+++ aicp.mk
@@ -1,2 +1,4 @@
 $(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 $(call inherit-product, device/aicp/sepolicy/common/sepolicy.mk)
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
+$(call inherit-product, device/aicp/sepolicy/common/sepolicy.mk)
